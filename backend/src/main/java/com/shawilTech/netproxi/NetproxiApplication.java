a246f7a5f3c2cfe3d40195cfc5b6360c967d6932
aocho <aocholayoul9@gmail.com>
2026-05-11 17:40:10 +0200

    feat: Add CommandLineRunner to initialize roles in the application

--- backend/src/main/java/com/shawilTech/netproxi/NetproxiApplication.java
+++ backend/src/main/java/com/shawilTech/netproxi/NetproxiApplication.java
@@ -1,14 +1,42 @@
 package com.shawilTech.netproxi;
 
-
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import com.shawilTech.netproxi.entity.Role;
+import com.shawilTech.netproxi.repository.RoleRepository;
 
 @SpringBootApplication(scanBasePackages = "com.shawilTech")
 public class NetproxiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NetproxiApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initRoles(RoleRepository repo) {
+		return args -> initRolesData(repo);
+	}
+
+	@Transactional
+	public void initRolesData(RoleRepository repo) {
+		List<String> roles = List.of(
+			"ROLE_SUPER_ADMIN",
+			"ROLE_COMPANY",
+			"ROLE_EMPLOYEE",
+			"ROLE_CLIENT"
+		);
+
+		for (String r : roles) {
+			repo.findByName(r).orElseGet(() ->
+				repo.save(Role.builder().name(r).build())
+			);
+		}
 	}
 
 }
